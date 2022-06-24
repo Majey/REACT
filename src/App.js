f6caa3c47d5ed1058d5e7031157c93534f6adc35
Majey <kevinerick78@yahoo.com>
2022-06-24 22:15:49 +0300

    useReducer()

--- src/App.js
+++ src/App.js
@@ -3,37 +3,18 @@ import { useReducer } from 'react';
 import './App.css';
 
 const ACTIONS = {
-  INCREMENT: "increment",
-  DECREMENT: "decrement"
 }
 
 function reducer(state, action) {
-  switch (action.type) {
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1};
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1};
-    default:
-      return state;
-  }
+  
 }
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  let increment = () => dispatch({ type: ACTIONS.INCREMENT })
-  let decrement = () => dispatch({ type: ACTIONS.DECREMENT})
-
-  // let [count, setCount] = useState(0);
-  // let increment = () => setCount(count += 1);
-  // let decrement = () => setCount(count -= 1);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="App">
-      <button className="increment" onClick={increment}>+</button>
-      <h1>{ state.count }</h1>
-      <button className="decrement" onClick={decrement}>-</button>
-    </div>
+    
   );
 }
 
